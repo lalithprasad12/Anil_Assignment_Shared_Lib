783521411605252160eb6967aef9ddb3d738ea81
lalithprasad12 <61561469+lalithprasad12@users.noreply.github.com>
2020-03-24 19:22:50 -0400

    Update msPipeline.groovy

--- vars/msPipeline.groovy
+++ vars/msPipeline.groovy
@@ -1,28 +1,26 @@
-#!/usr/bin/groovy
 
-def workspace
 
 node
 {
-    
+
     stage('Static Code Analysis')
     {
         echo "Static Code Analysis"
     }
-    
+
      stage('Build')
     {
         echo "Build the code"
     }
-    
+
      stage('Unit Testing')
     {
         echo "Unit Testing"
     }
-    
+
      stage('Deploy')
     {
         echo "Deploy the code"
     }
-    
+
 }
