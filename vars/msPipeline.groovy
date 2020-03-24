85ada730c334840fc5177755c185c3a980576f0e
lalithprasad12 <learn.lalith@gmail.com>
2020-03-24 19:15:36 -0400

    New groovy script1

--- vars/msPipeline.groovy
+++ vars/msPipeline.groovy
@@ -4,12 +4,6 @@ def workspace
 
 node
 {
-    stage('Checkout')
-    {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'PAC1', url: 'git@github.com:lalithprasad12/Anil_Assignment.git']]])
-        workspace=pwd()
-        
-    }
     
     stage('Static Code Analysis')
     {
